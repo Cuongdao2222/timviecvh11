76e03d627d9bb86b8cd3fb77607fa48f1208e5f9
Cuongdao2222 <cuongdao2692@gmail.com>
2022-06-05 21:15:41 +0700

    update code

--- app/Http/Controllers/employerRegisterController.php
+++ app/Http/Controllers/employerRegisterController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\employer_register;
 
 class employerRegisterController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $input = $request->all();
+
+    }
 }
