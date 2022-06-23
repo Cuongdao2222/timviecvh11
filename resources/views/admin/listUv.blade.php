df135fa499e62d62289d5aaf7f21fd3e527c4555
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-06-23 14:41:01 +0700

    update code

---
+++ resources/views/admin/listUv.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            }
+            td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+            }
+            tr:nth-child(even) {
+            background-color: #dddddd;
+            }
+        </style>
+    </head>
+    <body>
+
+        <h2>Danh sách ứng viên</h2>
+
+
+        <table>
+
+            <tr>
+                <th>Tên ứng viên</th>
+                <th>Cv ứng viên</th>
+                <th>Lịch sử nộp cv</th>
+                <th>Chọn nổi bật</th>
+                <th>Sửa</th>
+                <th>Xóa</th>
+            </tr>
+            <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+                <td>chọn</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+            </tr>
+            
+        </table>
+    </body>
+</html>
