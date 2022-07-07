66b8de72f6014c214f8bdf82fd043b125eb4736d
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-07-07 16:45:39 +0700

    update code

---
+++ app/Http/Controllers/employerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\employ_info;
+
+use DB;
+
+class employerController extends Controller
+{
+    protected function employDetails($link)
+    {
+        $findID = employ_info::where('links', $link)->first();
+
+        if(empty($findID)){
+            return abort('404');
+        }
+        $id_employ = $findID->id;
+
+        $data = DB::table('job')->join('employ_info', 'employ_info.employ_id', '=', 'job.employer_id')->where('employ_info.id', $id_employ)->get();
+
+       
+
+        return view('frontend.employ-detail-job', compact('data'));
+
+
+    }
+}
