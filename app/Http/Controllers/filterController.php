3d939d6a07eafc85e8067e9e36e7cf382f7b59ac
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-07-09 08:25:53 +0700

    update code

--- app/Http/Controllers/filterController.php
+++ app/Http/Controllers/filterController.php
@@ -23,13 +23,8 @@ class filterController extends Controller
 
         $industry = $request->industry;
 
-
         $job = DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')->where('job.title','LIKE', '%'. $datas .'%')->where('job.address_job', $address)->where('career', $industry)->OrWhere('employ_info.name', 'LIKE', '%' . $datas . '%')->where('job.address_job', $address)->where('career', $industry)->get();
 
-       
-
         return view('frontend.filter', compact('job'));
-
-
     }
 }
