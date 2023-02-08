31d6cc624153ed5a3add04020d77241960c3ca5c
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2023-02-08 11:26:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,9 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
