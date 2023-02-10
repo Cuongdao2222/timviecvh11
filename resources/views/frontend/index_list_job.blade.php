7c3efb41cacdcdb434addb8dfc4d8bfac3d90184
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2023-02-10 09:07:23 +0700

    Update index_list_job.blade.php

--- resources/views/frontend/index_list_job.blade.php
+++ resources/views/frontend/index_list_job.blade.php
@@ -170,10 +170,10 @@
                     <div class="logo"><a href="vi"><img alt="Công Ty Cổ Phần Eurowindow" src="{{ asset('images/logo.png') }}" /></a></div>
                     <div class="menu">
                         <ul>
-                            <li class="{{ route('employers_login_fe') }}"><a href="#" id="menu_11974"  target="_self">Đăng nhập </a>
+                            <li class=""><a href="{{ route('employers_login_fe') }}" id="menu_11974"  target="_self">Đăng nhập </a>
                             </li>
                             
-                            <li class="{{ route('form_recruit') }}"><a href="lien-he/vi" id="menu_11979"  target="_self">Đăng tin tuyển dụng </a>
+                            <li class=""><a href="{{ route('form_recruit') }}" id="menu_11979"  target="_self">Đăng tin tuyển dụng </a>
                             </li>
                         </ul>
                     </div>
